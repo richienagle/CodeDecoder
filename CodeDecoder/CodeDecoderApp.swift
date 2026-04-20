531d7c09cf9cb18157fbb5e1f6104835a7cd2e60
richie <richienagle@gmail.com>
2026-04-19 17:04:49 -0700

    Initial Commit

---
+++ CodeDecoder/CodeDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeDecoderApp.swift
+//  CodeDecoder
+//
+//  Created by Richard Nagle on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeDecoder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeDecoder
+//
+//  Created by Richard Nagle on 4/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
